ff99c24f31cee4664546620ef3373d788463a68b
Thomas Dufourd <thomas.dufourd@gmail.com>
2021-02-24 16:44:15 +0100

    Opprydding (fjerner gammel migration fil)

--- src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/App.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/App.kt
@@ -1,15 +1,16 @@
 package no.nav.arbeidsgiver.iatjenester.metrikker
 
 import io.github.cdimascio.dotenv.dotenv
+import no.nav.arbeidsgiver.iatjenester.metrikker.utils.log
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration
 import org.springframework.boot.runApplication
 
 private val environment = dotenv { ignoreIfMissing = true }
-
 @SpringBootApplication(exclude = [FlywayAutoConfiguration::class])
 class App
 
 fun main(args: Array<String>) {
+    log("main()").info("Starter ia-tjenester-metrikker applikasjon")
     runApplication<App>(*args)
 }
