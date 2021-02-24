47a28c61b397badefd6f0ed8ca5b65eafc78d350
Thomas Dufourd <thomas.dufourd@gmail.com>
2021-02-24 12:54:10 +0100

    Innføre spring-boot i applikasjon

---
+++ src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/DiagnosticsController.kt
@@ -0,0 +1,22 @@
+package no.nav.arbeidsgiver.iatjenester.metrikker
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class DiagnosticsController {
+
+    @GetMapping("/ping")
+    fun ping(): ResponseEntity<Unit> {
+        return ResponseEntity.ok().build()
+    }
+    @GetMapping("/internal/isalive")
+    fun isalive(): ResponseEntity<String> {
+        return ResponseEntity.ok("Is alive")
+    }
+    @GetMapping("/internal/isready")
+    fun isready(): ResponseEntity<String> {
+        return ResponseEntity.ok("Is ready")
+    }
+}
