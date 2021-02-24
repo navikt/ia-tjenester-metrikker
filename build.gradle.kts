27f50270de4e21f02a5fcc7637044707569c7c7d
Thomas Dufourd <thomas.dufourd@gmail.com>
2021-02-24 13:04:54 +0100

    Legg til 'shadow' plugin i build

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.4.3"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
     kotlin("jvm") version "1.4.30"
     kotlin("plugin.spring") version "1.4.30"
 
