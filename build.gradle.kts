feb2a91699004bb950bb3d89036674dd4013ed9a
Thomas Dufourd <thomas.dufourd@gmail.com>
2021-02-25 17:39:29 +0100

    Enable 'all-open' plugin så Spring annotations får open classes med kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.springframework.boot") version "2.4.3"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.4.30"
     kotlin("jvm") version "1.4.30"
     kotlin("plugin.spring") version "1.4.30"
     application
