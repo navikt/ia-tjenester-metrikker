4a1744df7eb976a8890785c9e53a38e3e9ddb8f4
erlendaxelsson <erlend.axelsson@gmail.com>
2021-02-28 17:15:34 +0100

    Implement kafka

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,8 @@ dependencies {
     implementation("org.postgresql:postgresql:42.2.18")
     implementation("com.h2database:h2:1.4.200")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.retry:spring-retry")
+    implementation("org.springframework.kafka:spring-kafka")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation ("org.postgresql:postgresql:42.2.18")
--- src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/App.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/App.kt
@@ -1,11 +1,14 @@
 package no.nav.arbeidsgiver.iatjenester.metrikker
 
 import io.github.cdimascio.dotenv.dotenv
+import no.nav.arbeidsgiver.iatjenester.metrikker.config.OutboundKafkaProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 private val environment = dotenv { ignoreIfMissing = true }
 
+@EnableConfigurationProperties(value = [OutboundKafkaProperties::class])
 @SpringBootApplication
 class App
 fun main(args: Array<String>) {
