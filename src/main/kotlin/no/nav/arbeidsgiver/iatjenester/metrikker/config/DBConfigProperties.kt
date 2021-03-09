62b91d7753a33ce5089be90a158e61c21662d038
Thomas Dufourd <thomas.dufourd@gmail.com>
2021-03-09 13:21:20 +0100

    Bruk bare Hikari datasource some config

--- src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/config/DBConfigProperties.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/config/DBConfigProperties.kt
@@ -1,13 +1,11 @@
 package no.nav.arbeidsgiver.iatjenester.metrikker.config
 
+import com.zaxxer.hikari.HikariConfig
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConstructorBinding
 @ConfigurationProperties(prefix = "spring.datasource")
 data class DBConfigProperties(
-    var url: String,
-    var username: String,
-    var password: String,
-    var driverClassName: String
+    var hikari: HikariConfig
 )
