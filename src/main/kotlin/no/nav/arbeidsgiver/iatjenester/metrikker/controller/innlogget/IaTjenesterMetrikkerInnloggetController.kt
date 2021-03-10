8652e71d3971ab612f10c9720249d27976d6c5d3
Thomas Dufourd <thomas.dufourd@gmail.com>
2021-03-10 20:33:41 +0100

    Innføre cors på 'mottatt-ia-tjeneste' endepunkter

--- src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/controller/innlogget/IaTjenesterMetrikkerInnloggetController.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/controller/innlogget/IaTjenesterMetrikkerInnloggetController.kt
@@ -4,14 +4,12 @@ import no.nav.arbeidsgiver.iatjenester.metrikker.controller.ResponseStatus
 import no.nav.arbeidsgiver.iatjenester.metrikker.domene.InnloggetIaTjeneste
 import no.nav.arbeidsgiver.iatjenester.metrikker.service.IaTjenesterMetrikkerService
 import no.nav.security.token.support.core.api.Protected
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 
 @Protected
 @RestController
+@CrossOrigin(origins = ["https://arbeidsgiver-q.nav.no", "https://arbeidsgiver.labs.nais.io"], allowCredentials = "true")
 @RequestMapping("/innlogget")
 class IaTjenesterMetrikkerInnloggetController(private val iaTjenesterMetrikkerService: IaTjenesterMetrikkerService) {
 
