adfdcc9aae99fcba90daffba3919fdd6a9633fb6
Thomas Dufourd <thomas.dufourd@gmail.com>
2021-03-12 07:57:29 +0100

    Enable cors på mottatt iatjenester endepunkter (#14)
    * Innføre cors på 'mottatt-ia-tjeneste' endepunkter
    
    * Deploy branch
    
    * Uinnlogget endepunktet vil ikke motta ikke credentials

--- src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/controller/uinnlogget/IaTjenesterMetrikkerUinnloggetController.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/controller/uinnlogget/IaTjenesterMetrikkerUinnloggetController.kt
@@ -4,14 +4,12 @@ import no.nav.arbeidsgiver.iatjenester.metrikker.controller.ResponseStatus
 import no.nav.arbeidsgiver.iatjenester.metrikker.domene.UinnloggetIaTjeneste
 import no.nav.arbeidsgiver.iatjenester.metrikker.service.IaTjenesterMetrikkerService
 import no.nav.security.token.support.core.api.Unprotected
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 
 @Unprotected
 @RestController
+@CrossOrigin(origins = ["https://arbeidsgiver-q.nav.no", "https://arbeidsgiver.labs.nais.io"], allowCredentials = "false")
 @RequestMapping("/uinnlogget")
 class IaTjenesterMetrikkerUinnloggetController(private val iaTjenesterMetrikkerService: IaTjenesterMetrikkerService) {
 
