c5cec479c26bf46a4cb1563f1c9db6d63782d8de
Thomas Dufourd <thomas.dufourd@gmail.com>
2021-03-15 12:41:53 +0100

    Disable Kafka config i PROD

--- src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/KafkaService.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/KafkaService.kt
@@ -4,9 +4,11 @@ import com.google.gson.Gson
 import no.nav.arbeidsgiver.iatjenester.metrikker.config.OutboundKafkaProperties
 import no.nav.arbeidsgiver.iatjenester.metrikker.domene.InnloggetIaTjeneste
 import no.nav.arbeidsgiver.iatjenester.metrikker.utils.log
+import org.springframework.context.annotation.Profile
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 
+@Profile("dev-gcp")
 @Service
 class KafkaService(private var kafkaTemplate: KafkaTemplate<String, String>, private var outboundKafkaProperties: OutboundKafkaProperties) {
 
