66fd24760f593334b9757c1ead2f0c808c445ebd
erlend-axelsson <32471637+erlend-axelsson@users.noreply.github.com>
2021-04-08 15:17:10 +0200

    Oppdatere avhengigheter april 2021 (#19)
    * La til gradle-versions-plugin
    
    * Fjernet bruk av deprecated mainClassName da vi ikke lenger
    bruker shadowJar
    
    * Oppdatert avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.3"
+    id("org.springframework.boot") version "2.4.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.4.30"
-    kotlin("jvm") version "1.4.30"
-    kotlin("plugin.spring") version "1.4.30"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.4.32"
+    id("com.github.ben-manes.versions") version "0.38.0"
+    kotlin("jvm") version "1.4.32"
+    kotlin("plugin.spring") version "1.4.32"
     application
 }
 
-val navSecurityVersion = "1.3.3"
+val navSecurityVersion = "1.3.4"
 val altinnRettigheterProxyKlientVersion = "2.0.1"
 
 group = "no.nav.arbeidsgiver"
@@ -17,8 +18,7 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 application {
-    // Det kommer en fix på denne: https://github.com/johnrengelman/shadow/issues/609
-    mainClassName = "no.nav.arbeidsgiver.iatjenester.metrikker.AppKt"
+    mainClass.set("no.nav.arbeidsgiver.iatjenester.metrikker.AppKt")
 }
 
 tasks.withType<KotlinCompile> {
@@ -47,8 +47,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springdoc:springdoc-openapi-ui:1.5.2")
-    implementation("org.springdoc:springdoc-openapi-kotlin:1.5.2")
+    implementation("org.springdoc:springdoc-openapi-ui:1.5.6")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.5.6")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
@@ -57,30 +57,29 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
 
     implementation("ch.qos.logback:logback-classic:1.2.3")
-    implementation("net.logstash.logback:logstash-logback-encoder:6.3")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.0")
+    implementation("net.logstash.logback:logstash-logback-encoder:6.6")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.2")
     implementation("com.google.code.gson:gson:2.8.6")
 
-    implementation("com.zaxxer:HikariCP:3.3.0")
-    implementation("org.flywaydb:flyway-core:5.0.2")
-    implementation("org.postgresql:postgresql:42.2.18")
+    implementation("com.zaxxer:HikariCP:3.4.5")
+    implementation("org.flywaydb:flyway-core:5.2.4")
+    implementation("org.postgresql:postgresql:42.2.19")
     implementation("com.h2database:h2:1.4.200")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.retry:spring-retry")
-    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springframework.kafka:spring-kafka:2.6.7")
     implementation("no.nav.security:token-validation-spring:${navSecurityVersion}")
     implementation("no.nav.arbeidsgiver:altinn-rettigheter-proxy-klient:${altinnRettigheterProxyKlientVersion}:kotlin-client")
 
     // Test dependencies
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("no.nav.security:token-validation-spring-test:${navSecurityVersion}")
-    testImplementation("io.rest-assured:spring-mock-mvc") // TODO: sjekk om denne er tatt i bruk
-    testImplementation("com.github.tomakehurst:wiremock:2.24.1")
+    testImplementation("com.github.tomakehurst:wiremock:2.27.2")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-    testImplementation("io.mockk:mockk:1.10.5")
-    testImplementation("org.assertj:assertj-core:3.18.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.0.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("io.mockk:mockk:1.11.0")
+    testImplementation("org.assertj:assertj-core:3.19.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
 }
 
