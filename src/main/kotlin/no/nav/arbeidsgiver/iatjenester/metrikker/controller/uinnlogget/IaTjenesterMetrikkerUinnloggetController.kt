680ba9640ea3eae7f4d989341146fa78f3d6455b
erlend-axelsson <32471637+erlend-axelsson@users.noreply.github.com>
2021-04-19 10:24:15 +0200

    Add Nav Call id to MDC in controllers (#20)

--- src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/controller/uinnlogget/IaTjenesterMetrikkerUinnloggetController.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/controller/uinnlogget/IaTjenesterMetrikkerUinnloggetController.kt
@@ -4,12 +4,16 @@ import no.nav.arbeidsgiver.iatjenester.metrikker.controller.ResponseStatus
 import no.nav.arbeidsgiver.iatjenester.metrikker.domene.UinnloggetIaTjeneste
 import no.nav.arbeidsgiver.iatjenester.metrikker.service.IaTjenesterMetrikkerService
 import no.nav.arbeidsgiver.iatjenester.metrikker.utils.log
+import no.nav.arbeidsgiver.iatjenester.metrikker.utils.setNavCallid
+import no.nav.arbeidsgiver.iatjenester.metrikker.utils.clearNavCallid
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.http.HttpHeaders
 
 
 @Unprotected
@@ -22,10 +26,12 @@ import org.springframework.web.bind.annotation.CrossOrigin
 class IaTjenesterMetrikkerUinnloggetController(private val iaTjenesterMetrikkerService: IaTjenesterMetrikkerService) {
 
     @PostMapping(value = ["/mottatt-iatjeneste"], consumes = ["application/json"], produces = ["application/json"])
-    fun leggTilNyMottattIaTjeneste(@RequestBody uinnloggetIaTjeneste: UinnloggetIaTjeneste): ResponseStatus {
+    fun leggTilNyMottattIaTjeneste(@RequestHeader headers: HttpHeaders, @RequestBody uinnloggetIaTjeneste: UinnloggetIaTjeneste): ResponseStatus {
+        setNavCallid(headers)
         log("IaTjenesterMetrikkerUinnloggetController")
             .info("Mottatt IA tjeneste (uinnlogget) fra ${uinnloggetIaTjeneste.kilde.name}")
         iaTjenesterMetrikkerService.sjekkOgOpprett(uinnloggetIaTjeneste)
+        clearNavCallid()
         return ResponseStatus.Created
     }
 }
