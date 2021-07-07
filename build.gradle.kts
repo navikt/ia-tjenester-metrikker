42786d5853905421e4337fb199fb1adc973445b1
Thomas Dufourd <thomas.dufourd@gmail.com>
2021-07-07 14:38:02 +0200

    Rydde opp i byggfil

--- build.gradle.kts
+++ build.gradle.kts
@@ -89,10 +89,3 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
 }
-
-/*configurations.all {
-    resolutionStrategy {
-        force("io.github.microutils:kotlin-logging:2.0.6")
-    }*/
-
-
