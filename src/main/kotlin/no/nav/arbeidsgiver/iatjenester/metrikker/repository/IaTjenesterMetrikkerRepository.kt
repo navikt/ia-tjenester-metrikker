07ba9df7cd11cfa2101f550dbb4eebfe9512a1ca
runarskagestad <runar.skagestad@nav.no>
2021-10-15 16:16:44 +0200

    La til test for at fylke blir satt i sjekkOgPersister. Forenklet testklassen ved å fjerne avhengigheter til JDBC-biblioteker

--- src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/repository/IaTjenesterMetrikkerRepository.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/repository/IaTjenesterMetrikkerRepository.kt
@@ -15,8 +15,9 @@ import java.time.LocalDateTime
 
 @Transactional
 @Repository
-class IaTjenesterMetrikkerRepository(private val namedParameterJdbcTemplate: NamedParameterJdbcTemplate) {
-
+class IaTjenesterMetrikkerRepository(
+    private val namedParameterJdbcTemplate: NamedParameterJdbcTemplate
+) {
 
     fun persister(iatjeneste: InnloggetMottattIaTjenesteMedVirksomhetGrunndata) {
         insertIaTjeneste(iatjeneste)
@@ -87,9 +88,15 @@ class IaTjenesterMetrikkerRepository(private val namedParameterJdbcTemplate: Nam
                 .addValue("naering_kode_5siffer", iatjeneste.næringKode5Siffer)
                 .addValue("form_av_tjeneste", iatjeneste.type.name)
                 .addValue("kilde_applikasjon", iatjeneste.kilde.name)
-                .addValue("tjeneste_mottakkelsesdato", iatjeneste.tjenesteMottakkelsesdato.toLocalDateTime())
+                .addValue(
+                    "tjeneste_mottakkelsesdato",
+                    iatjeneste.tjenesteMottakkelsesdato.toLocalDateTime()
+                )
                 .addValue("antall_ansatte", iatjeneste.antallAnsatte)
-                .addValue("naering_kode5siffer_beskrivelse", iatjeneste.næringskode5SifferBeskrivelse)
+                .addValue(
+                    "naering_kode5siffer_beskrivelse",
+                    iatjeneste.næringskode5SifferBeskrivelse
+                )
                 .addValue("naering_2siffer_beskrivelse", iatjeneste.næring2SifferBeskrivelse)
                 .addValue("ssb_sektor_kode", iatjeneste.SSBSektorKode)
                 .addValue("ssb_sektor_kode_beskrivelse", iatjeneste.SSBSektorKodeBeskrivelse)
@@ -113,12 +120,16 @@ class IaTjenesterMetrikkerRepository(private val namedParameterJdbcTemplate: Nam
         override val tidspunkt: LocalDateTime
     ) : MottattIaTjenesteMetrikk()
 
-    data class MottattUinnloggetIaTjenesteMetrikk(val kilde: Kilde, override val tidspunkt: LocalDateTime) :
-        MottattIaTjenesteMetrikk()
+    data class MottattUinnloggetIaTjenesteMetrikk(
+        val kilde: Kilde,
+        override val tidspunkt: LocalDateTime
+    ) :
+            MottattIaTjenesteMetrikk()
 
 
     fun hentUinnloggetMetrikker(startDato: LocalDate): List<MottattUinnloggetIaTjenesteMetrikk> =
-        namedParameterJdbcTemplate.query("""
+        namedParameterJdbcTemplate.query(
+            """
                 select tjeneste_mottakkelsesdato, kilde_applikasjon 
                 from metrikker_ia_tjenester_uinnlogget 
                 where tjeneste_mottakkelsesdato >= :startDato
@@ -153,7 +164,8 @@ class IaTjenesterMetrikkerRepository(private val namedParameterJdbcTemplate: Nam
                     Næring(
                         rs.getString("naering_kode_5siffer"),
                         rs.getString("naering_kode5siffer_beskrivelse"),
-                        rs.getString("naering_2siffer_beskrivelse")),
+                        rs.getString("naering_2siffer_beskrivelse")
+                    ),
                     rs.getString("kommunenummer"),
                     rs.getString("kommune"),
                     rs.getDate("tjeneste_mottakkelsesdato").toLocalDate().atStartOfDay()
