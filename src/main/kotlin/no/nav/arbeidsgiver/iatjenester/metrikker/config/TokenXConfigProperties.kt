5ae84003fdb12b04f4a8c6e761c5d4a97aad16be
runarskagestad <runar.skagestad@nav.no>
2022-05-19 14:18:46 +0200

    La til token_endpoint i TokenXConfigProperties

--- src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/config/TokenXConfigProperties.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/config/TokenXConfigProperties.kt
@@ -7,12 +7,25 @@ import org.springframework.boot.context.properties.ConstructorBinding
 @ConfigurationProperties(prefix = "tokenx")
 data class TokenXConfigProperties(
     val tokendingsUrl: String = "Tokendings URL (WELL_KNOWN_URL)",
-    val clientId: String = "cluster:namespace:ia-tjenester-metrikker",
-    val altinnRettigheterProxyAudience: String = "cluster:namespace:altinn-rettigheter-proxy"
+    val clientId: String = "Client ID that uniquely identifies the application in TokenX. It has the format ´cluster:namespace:altinn-rettigheter-proxy`",
+    val privateJwk: String = "Private JWK containing an RSA key belonging to your client. Used to sign client assertions during client authentication.",
+    val tokenEndpoint: String = "Token endpoint, eg. `https://tokendings.dev-gcp.nais.io/token`",
+    val altinnRettigheterProxyAudience: String = "",
 ) {
     init {
-        require(tokendingsUrl.startsWith("http")) { "Ingen URL uten http, takk!" }
+        require(tokendingsUrl.toBeValidUrl()) { "Ikke en gyldig URL" }
         require(clientId.matches(Regex("^.+:.+:.+$"))) { "$this.clientId er ikke på gyldig format" }
+        require(tokenEndpoint.toBeValidUrl()) { "Ikke en gyldig URL" }
+        require(privateJwk.toContainCorrectClaims())
         require(altinnRettigheterProxyAudience.matches(Regex("^.+:.+:.+$"))) { "$this.altinnRettigheterProxyAudience er ikke på gyldig format" }
     }
+}
+
+fun String.toBeValidUrl(): Boolean {
+    // TODO: Implement a proper validator
+    return this.startsWith("http")
+}
+
+fun String.toContainCorrectClaims(): Boolean {
+    return this.contains("use") && this.contains("kty") && this.contains("kid")
 }
