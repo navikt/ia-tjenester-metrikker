75ffbd979d2246217c5a8d3683f25082beb7b6a7
runarskagestad <runar.skagestad@nav.no>
2022-05-19 13:21:48 +0200

    La til altinnRettigheterProxyAudience i TokenXConfigProperties.kt

--- src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/config/TokenXConfigProperties.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/config/TokenXConfigProperties.kt
@@ -7,10 +7,12 @@ import org.springframework.boot.context.properties.ConstructorBinding
 @ConfigurationProperties(prefix = "tokenx")
 data class TokenXConfigProperties(
     val tokendingsUrl: String = "Tokendings URL (WELL_KNOWN_URL)",
-    val clientId: String = "Client ID (<cluster>:<metadata.namespace>:<metadata.name>)"
+    val clientId: String = "cluster:namespace:ia-tjenester-metrikker",
+    val altinnRettigheterProxyAudience: String = "cluster:namespace:altinn-rettigheter-proxy"
 ) {
     init {
         require(tokendingsUrl.startsWith("http")) { "Ingen URL uten http, takk!" }
-        require(clientId.matches(Regex("^.+:.+:.+$"))) { "$this.clientId er ikke en gyldig clientId" }
+        require(clientId.matches(Regex("^.+:.+:.+$"))) { "$this.clientId er ikke på gyldig format" }
+        require(altinnRettigheterProxyAudience.matches(Regex("^.+:.+:.+$"))) { "$this.altinnRettigheterProxyAudience er ikke på gyldig format" }
     }
 }
