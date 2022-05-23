f9850d9578adceef5d40a5bd8ffdd342b42349c6
runarskagestad <runar.skagestad@nav.no>
2022-05-23 16:16:58 +0200

    Fjernet ubrukt config-fil, finset format på dummy-verdi i application-local/test.yaml

--- src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/config/TokenXClientProperties.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.arbeidsgiver.iatjenester.metrikker.config
-
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-class TokenXClientProperties(private val tokenXConfigProperties: TokenXConfigProperties) {
-
-}
