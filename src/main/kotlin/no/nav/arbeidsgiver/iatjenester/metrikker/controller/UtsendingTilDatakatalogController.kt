5b26530bab6dd4994188372fd6ac1abe797d05da
Malaz Alkoj <malaz.alkoj@nav.no>
2022-06-10 11:32:39 +0200

    enable midlertidig utsending av metrikker

--- src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/controller/UtsendingTilDatakatalogController.kt
+++ src/main/kotlin/no/nav/arbeidsgiver/iatjenester/metrikker/controller/UtsendingTilDatakatalogController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*
 
 @Unprotected
 @RestController
-@Profile("local", "dev-gcp")
+@Profile("local", "dev-gcp", "prod-gcp")
 @RequestMapping("/utsending")
 class UtsendingTilDatakatalogController(val datakatalogStatistikk: DatakatalogStatistikk) {
 
