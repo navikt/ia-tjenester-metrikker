faf3e06bc2432cddd53fab29efccfbb960ed51ea
Runar <runska@gmail.com>
2023-07-26 17:07:54 +0200

    Remove redundant deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,6 @@ val navTokenSupportVersion = "3.1.0"
 val shedlockVersion = "5.6.0"
 val springdocOpenapiVersion = "1.7.0"
 val log4jVersion = "2.20.0"
-val jupiterVersion = "5.7.2"
 
 group = "no.nav.arbeidsgiver"
 version = "0.0.1-SNAPSHOT"
@@ -54,29 +53,23 @@ repositories {
 dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
     implementation("org.springdoc:springdoc-openapi-ui:$springdocOpenapiVersion")
     implementation("org.springdoc:springdoc-openapi-kotlin:$springdocOpenapiVersion")
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("ch.qos.logback:logback-classic:1.4.8")
     implementation("net.logstash.logback:logstash-logback-encoder:7.4")
-    implementation("org.apache.logging.log4j:log4j-api:${log4jVersion}")
-    implementation("org.apache.logging.log4j:log4j-core:${log4jVersion}")
-    implementation("org.apache.logging.log4j:log4j-to-slf4j:2.16.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
-    implementation("com.google.code.gson:gson:2.10.1")
     implementation("io.arrow-kt:arrow-core:1.2.0")
-    implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("org.flywaydb:flyway-core:9.21.0")
     implementation("org.postgresql:postgresql:42.6.0")
     implementation("com.h2database:h2:2.2.220")
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.retry:spring-retry")
     implementation("no.nav.security:token-validation-spring:${navTokenSupportVersion}")
     implementation("no.nav.arbeidsgiver:altinn-rettigheter-proxy-klient:3.1.0")
@@ -85,18 +78,13 @@ dependencies {
     implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:$shedlockVersion")
     implementation("io.micrometer:micrometer-registry-prometheus:1.11.2")
     implementation("com.github.tomakehurst:wiremock:3.0.0-beta-10")
-    implementation ("io.prometheus:simpleclient:0.16.0")
 
     compileOnly("jakarta.servlet:jakarta.servlet-api:6.0.0")
 
     // Test dependencies
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("no.nav.security:token-validation-spring-test:${navTokenSupportVersion}")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.assertj:assertj-core:3.24.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${jupiterVersion}")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.2")
+    testImplementation("no.nav.security:token-validation-spring-test:${navTokenSupportVersion}")
     testImplementation("io.mockk:mockk:1.13.5")
     testImplementation("org.springframework.cloud:spring-cloud-starter-contract-stub-runner:4.0.3")
-
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${jupiterVersion}")
 }
