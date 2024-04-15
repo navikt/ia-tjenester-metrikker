8ae3b4f2b67c4a59d817e43845c36f29e0cb6e38
Thomas Dufourd <thomas.dufourd@gmail.com>
2024-04-15 16:24:47 +0200

    Oppdater altinn-rettigheter-proxy-klient til siste versjon (4.0.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ dependencies {
     implementation("org.postgresql:postgresql:42.7.3")
     implementation("org.springframework.retry:spring-retry")
     implementation("no.nav.security:token-validation-spring:${navTokenSupportVersion}")
-    implementation("no.nav.arbeidsgiver:altinn-rettigheter-proxy-klient:3.1.0")
+    implementation("no.nav.arbeidsgiver:altinn-rettigheter-proxy-klient:4.0.0")
     implementation("io.micrometer:micrometer-registry-prometheus:1.12.5")
 
     compileOnly("jakarta.servlet:jakarta.servlet-api:6.0.0")
