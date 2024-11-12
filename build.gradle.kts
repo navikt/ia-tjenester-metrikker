d4d853da9184376f0e3dbadf8cbddaf2b1350258
Christian Klem <christian.klem@nav.no>
2024-11-12 13:09:21 +0100

    Oppdater avhengigheter
     - oppdater github action
     - spring -> 3.3.5
     - spring.dependency-managment -> 1.1.6
     - kotlin -> 2.0.21
     - fjern ubrukt plugin
     - diverse minor patcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,14 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.3.4"
-    id("io.spring.dependency-management") version "1.1.4"
-    id("com.github.ben-manes.versions") version "0.51.0"
-    kotlin("jvm") version "2.0.10"
-    kotlin("plugin.spring") version "2.0.20"
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.spring") version "2.0.21"
     application
 }
 
-val navTokenSupportVersion = "5.0.5"
+val navTokenSupportVersion = "5.0.11"
 val springdocOpenapiVersion = "1.8.0"
 val log4jVersion = "2.20.0"
 
@@ -71,13 +70,13 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-kotlin:$springdocOpenapiVersion")
     implementation("net.logstash.logback:logstash-logback-encoder:8.0")
     implementation("io.arrow-kt:arrow-core:1.2.4")
-    implementation("org.flywaydb:flyway-core:10.19.0")
-    implementation("org.flywaydb:flyway-database-postgresql:10.19.0")
+    implementation("org.flywaydb:flyway-core:10.21.0")
+    implementation("org.flywaydb:flyway-database-postgresql:10.21.0")
     implementation("org.postgresql:postgresql:42.7.4")
     implementation("org.springframework.retry:spring-retry")
     implementation("no.nav.security:token-validation-spring:$navTokenSupportVersion")
     implementation("no.nav.arbeidsgiver:altinn-rettigheter-proxy-klient:4.0.0")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.13.5")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.14.0")
 
     compileOnly("jakarta.servlet:jakarta.servlet-api:6.1.0")
 
@@ -86,7 +85,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("no.nav.security:token-validation-spring-test:$navTokenSupportVersion")
-    testImplementation("io.mockk:mockk:1.13.12")
+    testImplementation("io.mockk:mockk:1.13.13")
     testImplementation("org.springframework.cloud:spring-cloud-starter-contract-stub-runner:4.1.4")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
