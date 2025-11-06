ad6f377f98b37f7c13deaf2fb483d91d26a90ce8
Thomas Dufourd <thomas.dufourd@gmail.com>
2025-11-06 13:48:39 +0100

    Oppdater patch-version for noen avhengigheter: kotlin, springframework, prometheus, mockk og postgresql-connector

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.5.5"
+    id("org.springframework.boot") version "3.5.7"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("jvm") version "2.2.10"
+    kotlin("jvm") version "2.2.21"
     kotlin("plugin.spring") version "2.2.0"
     application
 }
@@ -12,10 +12,10 @@ plugins {
 val arrowKtVersion = "2.1.2"
 val flywayVersion = "11.11.2"
 val logbackEncoderVersion = "8.1"
-val mockkVersion = "1.14.5"
+val mockkVersion = "1.14.6"
 val navTokenSupportVersion = "5.0.30"
-val postgresqlVersion = "42.7.7"
-val prometheusVersion = "1.15.3"
+val postgresqlVersion = "42.7.8"
+val prometheusVersion = "1.15.5"
 val springCloudStubRunnerVersion = "4.3.0"
 val springdocOpenapiVersion = "1.8.0"
 
